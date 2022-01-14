1a4cb7acc60cd2674e1cbd987f77afb24a2fbaaa
Vikram Nadig <vikram.nadig@cazoo.co.uk>
2022-01-14 16:39:50 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/fibonacci.test.ts
+++ src/test/fibonacci.test.ts
@@ -2,7 +2,7 @@ const getFibonacci = (input: number): number => {
   if (input === 0 ){
     return 0;
   }
-  if (input >= 3){
+  if (input >= 2){
     return getFibonacci(input-1)+getFibonacci(input-2);
   }
   return 1;
@@ -15,6 +15,11 @@ describe.each([
   [3,2],
   [4,3],
   [5,5],
+  [6,8],
+  [7,13],
+  [8,21],
+  [9,34],
+  [10,55],
   ])('given %p as argument, returns %p', function (input:number, expected) {
 
   it('should get Fibonacci number', function () {
